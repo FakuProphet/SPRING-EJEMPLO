cdb76324fbed893c292bd63fbf05d823fe76ad8d
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2019-03-24 17:00:36 -0300

    creando HomeController

---
+++ src/java/controller/HomeController.java
@@ -0,0 +1,28 @@
+
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class HomeController {
+    
+    
+    /*este metodo es de una vista*/
+    /*recibe parametros para especificar como acceder por url*/
+    @RequestMapping("home.htm")
+    public ModelAndView home(){
+        /*metodo de vista */
+        /*
+            la clase ModelAndView permite renderizar
+            vistas como servlet al estilo screen.
+        */
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("home");
+        return mav;
+    }
+    
+}
